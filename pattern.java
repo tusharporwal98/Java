f927e14212c7b5b59b38101d9d04ce8f725f7f4a
tusharporwal98 <118659027+tusharporwal98@users.noreply.github.com>
2023-06-23 17:35:06 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,28 @@
+public class pattern {
+    public static void main(String[] args) {
+
+        //Inverted pyramid 
+      /*  for(int i=5;i>1;i--){
+            for(int j=1 ; j<=i-1 ;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */ 
+
+   
+   
+        //Half pyramid pattern (in no's)
+       /* int n =5;
+        for(int i=1; i<=n ; i++){
+            for(int numbers =1 ; numbers<=i ;numbers++){
+                System.out.print(numbers);
+            }
+            System.out.println();
+        }
+        */ 
+   
+   
+   
+    }
+}
